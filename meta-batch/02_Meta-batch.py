7666da9aee5afe9366e756c94d8a7c4cc1110a3c
yangsenius <465097485@qq.com>
2018-10-11 00:06:51 +0800

    good

--- meta-batch/02_Meta-batch.py
+++ meta-batch/02_Meta-batch.py
@@ -42,22 +42,26 @@ class MetaData_Container(object):
         assert len(loss)==self.batchsize
         for id, loss in enumerate(loss):
             if Batchmeta['index'][id] not in self.ID_list: #第一次碰到这个样本 epoch=0
-                #print("index{}".format(Batchmeta['index'][id]))
+                
+                print("index={},loss={}".format(Batchmeta['index'][id],loss))
                 tmp={'index':Batchmeta['index'][id], # identiy index
                     'memory_difficult':Batchmeta['memory_difficult'][id], # difficult value for each epoch
                     'forget_degree':Batchmeta['forget_degree'][id],
-                    'loss':loss} 
+                    'loss':loss,
+                    'level':888} 
                 Batchmeta_list.append(tmp)
                 self.ID_list.append(tmp['index']) #面向整个数据集建立 全局动态索引表
                 self.MetaData_Dict.append({'index':tmp['index'],
                                       'memory_difficult':tmp['memory_difficult'],
-                                      'forget_degree':tmp['forget_degree']}) # loss 不被加入到全局索引表中
+                                      'forget_degree':tmp['forget_degree'],
+                                      'level':tmp['level']}) # 初始难度均为888
             else: #epoch>=1 从索引表中加载数据
                 for ID, index in enumerate(self.ID_list):
                     #print('index=={}'.format(index))
                     if index == Batchmeta['index'][id]:
                         tmp=self.MetaData_Dict[ID]  #找到该样本在索引表中储存的META数据
                         tmp['loss']=loss
+                        #tmp['level']=1
                         Batchmeta_list.append(tmp)
         return Batchmeta_list
 
@@ -95,8 +99,9 @@ class MetaData_Container(object):
             # we should redefine the difficult for each batch example
             tmp['forget_degree'] = 0 if tmp['forget_degree'] < 0 else tmp['forget_degree']
             tmp['forget_degree'] = 100 if tmp['forget_degree'] > 100 else tmp['forget_degree']
-
-            tmp['memory_difficult'] = int(0.5*tmp['memory_difficult']+0.5*tmp['forget_degree'])
+            print('x,y=',tmp['forget_degree'],tmp['memory_difficult'])
+            tmp['memory_difficult'] = (tmp['memory_difficult']+tmp['forget_degree'])/2
+            print('y',tmp['memory_difficult'])
             new_minibatchmeta.append(tmp)
         #logger.info("minibatch_meta and backward_loss have been updated ")
         self.Gradient_Backward_Loss=minibatch_backward_loss
@@ -112,17 +117,18 @@ class MetaData_Container(object):
                 if tmp['index'] == index:
                     self.MetaData_Dict[id]['memory_difficult']=tmp['memory_difficult']
                     self.MetaData_Dict[id]['forget_degree']=tmp['forget_degree']
+                    self.MetaData_Dict[id]['level']=tmp['level']
 
     def Bulid_Table(self):
         """inital Table
         preserve all values in all epoches"""
         total_num,total_epoch=self.total_num,self.total_epoch
-        Table_Index_ID = torch.zeros(size=(total_num,total_epoch)) 
+        #Table_Index_ID = torch.zeros(size=(total_num,total_epoch)) 
         Table_Index_Difficult = torch.zeros(size=(total_num,total_epoch))
         Table_Index_Forget = torch.zeros(size=(total_num,total_epoch))
         Table_Index_Loss = torch.zeros(size=(total_num,total_epoch))
         Table_Index_Level = torch.zeros(size=(total_num,total_epoch))
-        Table={'index_table':Table_Index_ID,
+        Table={
                'difficult_table':Table_Index_Difficult,
                'forget_table':Table_Index_Forget,
                'loss_table':Table_Index_Loss,
@@ -134,13 +140,13 @@ class MetaData_Container(object):
         Table = self.table 
         for id in range(len(meta_batch)):
             index=meta_batch[id]['index']
-            Table['index_table'][index][epoch] = meta_batch[id]['index']
+            #Table['index_table'][index][epoch] = meta_batch[id]['index']
             Table['difficult_table'][index][epoch] = meta_batch[id]['memory_difficult']
             Table['forget_table'][index][epoch] = meta_batch[id]['forget_degree']
-            Table['loss_table'][index][epoch] = meta_batch[id]['loss'] if 'loss' in meta_batch[id] else 0.
-            Table['level_table'][index][epoch] = meta_batch[id]['level'] if 'level' in meta_batch[id] else 5
+            Table['loss_table'][index][epoch] = meta_batch[id]['loss'] 
+            Table['level_table'][index][epoch] = meta_batch[id]['level'] 
         self.table=Table
-        return Table
+        
         
 class Meta_dataset(Dataset):
     def __init__(self,l):
@@ -161,15 +167,15 @@ class Meta_dataset(Dataset):
 def train(train_loader,MetaData_Container, epoch):
     
     M_C=MetaData_Container
-
+    
     for i, (loss, meta) in enumerate(train_loader):
         
         Batchmeta = M_C.MiniBatch_MetaData_Loader(loss,meta) 
-        table=M_C.Update_Table_Index(epoch,Batchmeta)
+        M_C.Update_Table_Index(epoch,Batchmeta)
 
-        new_batchmeta = M_C.Update_MiniBatch_MetaData(Batchmeta)
-
-        M_C.Update_New_Minibatch_To_MetaData_Dict(new_batchmeta)
+        new_Batchmeta = M_C.Update_MiniBatch_MetaData(Batchmeta)
+        #M_C.Update_Table_Index(epoch,new_Batchmeta)
+        M_C.Update_New_Minibatch_To_MetaData_Dict(new_Batchmeta)
 
         loss=M_C.Gradient_Backward_Loss
 
